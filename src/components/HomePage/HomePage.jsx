377bee31a953b2e525e8843a151233018134fa27
Alexander Ruziska <ziskapants@gmail.com>
2025-02-27 15:08:52 -0600

    fixed bug on home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,7 +5,6 @@ import useStore from "../../zustand/store";
 function HomePage() {
   const user = useStore((state) => state.user);
   const logOut = useStore((state) => state.logOut);
-  const monsterImage = useStore((state) => state.monsterImage);
   const fetchMonster = useStore((state) => state.fetchMonsterById);
 
   useEffect(() => {
@@ -18,7 +17,6 @@ function HomePage() {
       <p>Your username is: {user.username}</p>
       <p>Your ID is: {user.id}</p>
       <button onClick={logOut}>Log Out</button>
-      <img src={monsterImage} alt="monster image" />
     </>
   );
 }
