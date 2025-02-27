7944d9fcfbcc6c0d886223b70a195b673ec1ee71
Alexander Ruziska <ziskapants@gmail.com>
2025-02-27 14:44:46 -0600

    made forseeable components

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useStore } from '../../zustand/store';
+// import { useStore } from '../../zustand/store';
 function Library() {
     const navigate = useNavigate();
-    const monsters = useStore((store) => store.monster);
-    const fetchMonsters = useStore((store) => store.fetchMonsters);
+    // const monsters = useStore((store) => store.monster);
+    // const fetchMonsters = useStore((store) => store.fetchMonsters);
 
 
-    useEffect(() => {
-        fetchMovies();
-      }, []);
+    // useEffect(() => {
+    //     fetchMovies();
+    //   }, []);
     
       const handleClick = (id) => {
         navigate(`/monster-card/${id}`);
@@ -20,7 +20,7 @@ function Library() {
             <main>
               <h1>Movie List</h1>
               <div className="monster-grid">
-                {monsters.map((monster) => (
+                {/* {monsters.map((monster) => (
                   <div className="monster-card" data-testid="monsterItem" key={monster.id}>
                     <img
                       src={monster.image_url}
@@ -29,7 +29,7 @@ function Library() {
                     />
                     <h5>{monster.name}</h5>
                   </div>
-                ))}
+                ))} */}
               </div>
         
               <div className="d-flex justify-content-center mt-4">
